304c67fbdaa6a8b1c42e198117281fd50fa256e0
David González <dgonmor428@g.educaand.es>
2026-04-14 20:52:42 +0200

    commit con las dependencias realizadas

--- simpsons.sql
+++ simpsons.sql
@@ -2,26 +2,30 @@
 Create database simpsons;
 use simpsons;
 
---Creación de la tabla Badaluque
-Create table Badaluque (
-
-
-)
+--Creación de la tabla Baduluque
+Create table Baduluque (
+  codigo INT,
+  nombre_productos VARCHAR(255) NOT NULL,
+  CONSTRAINT PRIMARY KEY (codigo)
+) ENGINE=INNODB;
 --Creación de la tabla Bar de Moe
 Create table Bar_Moe (
   codigo INT,
-  nombre VARCHAR(255) NOT NULL,
+  nombre_productos VARCHAR(255) NOT NULL,
   CONSTRAINT PRIMARY KEY (codigo)
 ) ENGINE=INNODB;
 --Creación de la tabla tienda de comics
 Create table Tienda_Comics (
-
-
-)
+  codigo INT,
+  nombre_comics VARCHAR(255) NOT NULL,
+  serie_comic VARCHAR(255) NOT NULL,
+  CONSTRAINT PRIMARY KEY (codigo)
+) ENGINE=INNODB;
 --Creación de la tabla personajes principales
 Create table Personajes_Principales (
-
-
-)
+  codigo INT,
+  nombre_productos VARCHAR(255) NOT NULL,
+  CONSTRAINT PRIMARY KEY (codigo)
+) ENGINE=INNODB;
 
 --Inserto 
