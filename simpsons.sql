9a56ccb1b52eb4887b2951ccab154bf75602fd08
David González <dgonmor428@g.educaand.es>
2026-04-14 20:37:13 +0200

    otro más

---
+++ simpsons.sql
@@ -0,0 +1,27 @@
+--Crear y usar la base de datos simpsons
+Create database simpsons;
+use simpsons;
+
+--Creación de la tabla Badaluque
+Create table Badaluque (
+
+
+)
+--Creación de la tabla Bar de Moe
+Create table Bar_Moe (
+  codigo INT,
+  nombre VARCHAR(255) NOT NULL,
+  CONSTRAINT PRIMARY KEY (codigo)
+) ENGINE=INNODB;
+--Creación de la tabla tienda de comics
+Create table Tienda_Comics (
+
+
+)
+--Creación de la tabla personajes principales
+Create table Personajes_Principales (
+
+
+)
+
+--Inserto 
