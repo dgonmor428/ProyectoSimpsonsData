973ea03f6fb2110131150a93ecd623ec099bd5c1
Jandro-21 <alejandrodurillovargas21@gmail.com>
2026-04-24 12:13:43 +0200

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,14 @@
+
+//Animación del ocapacity (es para que aparezca la página) 
+setTimeout(() => {
+            const contenedor = document.querySelector('body');
+            if (contenedor) contenedor.style.opacity = '1';
+        }, 100);
+setTimeout(() => {
+            const contenedor = document.querySelector('.badulaque-body');
+            if (contenedor) contenedor.style.opacity = '1';
+        }, 100); 
+setTimeout(() => {
+            const contenedor = document.querySelector('.bardemoe-body');
+            if (contenedor) contenedor.style.opacity = '1';
+        }, 100);
