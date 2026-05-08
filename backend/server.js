f4cc784d6c087e9570c96b10aa04df3362925d5f
Jandro-21 <alejandrodurillovargas21@gmail.com>
2026-05-08 10:09:45 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,28 @@
-const express = require("express");
-const mysql = require("mysql2");
-const cors = require("cors");
-const path = require("path");
+const express = require("express");          // Importa el framework para crear el servidor web 
+const mysql = require("mysql2");            // Importa el cliente para conectarnos a MySQL 
+const cors = require("cors");                // Importa el módulo CORS (Cross-Origin Resource Sharing), que permite que el servidor acepte solicitudes desde un dominio diferente al suyo 
 
-const server = express();
-server.use(cors());
-server.use(express.json());
+// Creamos una instancia de Express para nuestro servidor 
+const server = express(); 
+// Habilitamos las peticiones desde el frontend 
+ server.use(cors());            // Habilita CORS para evitar bloqueos en las peticiones del navegador 
+ server.use(express.json());    // Permite recibir datos en formato JSON en las peticiones 
+   
+ // Definimos las constantes necesarias para la conexión con el servidor 
+ const PORT = 3000;  // Puerto donde correrá nuestro servidor 
+   
+ // Creamos un pool de conexiones, que hará que las conexiones se hagan bajo demanda según se vayan necesitando 
+ const pool = mysql.createPool({ 
+     host: "localhost",              // Dirección del servidor 
+     port: 3306,                     // Puerto al que nos conectamos en MySQL 
+     user: "root",                   // Usuario al que nos conectamos 
+     password: "",                   // Contraseña del usuario al que nos conectamos 
+     database: "simpsons",          // Nombre de la base de datos que nos conectamos 
+     waitForConnections: true,       // Hace que las nuevas peticiones esperan en cola hasta que haya una conexión libre. Si vale false esas nuevas peticiones fallan 
 
-server.use(express.static(path.resolve(__dirname, "../")));
-
-const PORT = 3000;
-
-const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "simpsons"
-});
-
-
-
+     connectionLimit: 10,            // Define el máximo de conexiones simultáneas al servidor MySQL
+     queueLimit: 0                    // Define el límite de peticiones en espera. El valor 0 define una cola infinita
+ });
 server.get("/badulaque", (req, res) => {
     const nombre = req.query.nombre;
     let sql = "SELECT * FROM Badulaque";
@@ -40,11 +43,14 @@ server.get("/badulaque", (req, res) => {
 server.post("/badulaque", (req, res) => {
     const { codigo, nombre_producto } = req.body;
 
-    const sql = "INSERT INTO Badulaque VALUES (?, ?)";
+    const sql = "INSERT INTO Badulaque (codigo, nombre_producto) VALUES (?, ?)";
 
     pool.query(sql, [codigo, nombre_producto], (error) => {
         if (error) return res.status(500).json({ error });
-        res.json({ mensaje: "Insertado" });
+        res.json({
+             mensaje: "Producto insertado correctamente", 
+             datos: { codigo, nombre_producto } 
+        });
     });
 });
 
@@ -92,11 +98,14 @@ server.get("/barMoe", (req, res) => {
 server.post("/barMoe", (req, res) => {
     const { codigo, nombre_producto } = req.body;
 
-    const sql = "INSERT INTO Bar_Moe VALUES (?, ?)";
+     const sql = "INSERT INTO Bar_Moe (codigo, nombre_producto) VALUES (?, ?)";
 
     pool.query(sql, [codigo, nombre_producto], (error) => {
         if (error) return res.status(500).json({ error });
-        res.json({ mensaje: "Insertado" });
+        res.json({
+             mensaje: "Producto insertado correctamente", 
+             datos: { codigo, nombre_producto } 
+        });
     });
 });
 
@@ -123,8 +132,6 @@ server.delete("/barMoe/:codigo", (req, res) => {
     });
 });
 
-
-
 server.get("/tiendaComics", (req, res) => {
     const nombre = req.query.nombre;
     let sql = "SELECT * FROM Tienda_Comics";
@@ -144,11 +151,14 @@ server.get("/tiendaComics", (req, res) => {
 server.post("/tiendaComics", (req, res) => {
     const { codigo, nombre_comic, serie_comic, numero_paginas } = req.body;
 
-    const sql = "INSERT INTO Tienda_Comics VALUES (?, ?, ?, ?)";
+    const sql = "INSERT INTO Tienda_Comics (codigo, nombre_comic, serie_comic, numero_paginas) VALUES (?, ?, ?, ?)";
 
     pool.query(sql, [codigo, nombre_comic, serie_comic, numero_paginas], (error) => {
         if (error) return res.status(500).json({ error });
-        res.json({ mensaje: "Insertado" });
+        res.json({
+             mensaje: "Producto insertado correctamente", 
+             datos: { codigo, nombre_comic, serie_comic, numero_paginas } 
+        });
     });
 });
 
@@ -196,13 +206,16 @@ server.get("/personajes", (req, res) => {
 server.post("/personajes", (req, res) => {
     const { codigo_personaje, nombre, apellido1, madre, padre, actor_doblaje, primera_aparicion } = req.body;
 
-    const sql = "INSERT INTO Personajes_Principales VALUES (?, ?, ?, ?, ?, ?, ?)";
+    const sql = "INSERT INTO Personajes_Principales (codigo_personaje, nombre, apellido1, madre, padre, actor_doblaje, primera_aparicion) VALUES (?, ?, ?, ?, ?, ?, ?)";
 
     pool.query(sql,
         [codigo_personaje, nombre, apellido1, madre, padre, actor_doblaje, primera_aparicion],
         (error) => {
             if (error) return res.status(500).json({ error });
-            res.json({ mensaje: "Insertado" });
+            res.json({
+             mensaje: "Producto insertado correctamente", 
+             datos: { codigo_personaje, nombre, apellido1, madre, padre, actor_doblaje, primera_aparicion } 
+        });
         }
     );
 });
@@ -217,7 +230,7 @@ server.put("/personajes/:id", (req, res) => {
         [nombre, apellido1, madre, padre, actor_doblaje, primera_aparicion, id],
         (error) => {
             if (error) return res.status(500).json({ error });
-            res.json({ mensaje: "Actualizado" });
+            res.json({ mensaje: "Se ha actualizado correctamente" });
         }
     );
 });
@@ -228,13 +241,29 @@ server.delete("/personajes/:id", (req, res) => {
     const sql = "DELETE FROM Personajes_Principales WHERE codigo_personaje=?";
 
     pool.query(sql, [id], (error) => {
-        if (error) return res.status(500).json({ error });
-        res.json({ mensaje: "Eliminado" });
+        if (error){
+            console.error("Error en DELETE:", error);
+            return res.status(500).json({ error });
+        } 
+        res.json({ mensaje: "Se ha eliminado correctamente" });
     });
 });
 
 
+function iniciarServidor() { 
+    // Solo arrancamos el servidor cuando la BD está conectada 
+    pool.getConnection((error, connection) => { 
+        if (error) { 
+            console.error("Error conectando a MySQL:", error); 
+            process.exit(1); // Si falla la BD, cerramos el server 
+        } 
+        connection.release(); 
+        // Iniciamos el servidor en el puerto especificado 
+        server.listen(PORT, () => { 
+            // Confirmación en la consola de que se ha lanzado el servidor OK 
+            console.log(`Conectado a MySQL. Servidor corriendo en http://localhost:${PORT}`); 
+        }); 
+    }); 
+}
 
-server.listen(PORT, () => {
-    console.log(`Servidor en http://localhost:${PORT}`);
-});
+iniciarServidor(); //Para arrancar el servidor
