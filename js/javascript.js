1c6896bf8df6f82d9281dec65cc98c8dec66079a
Jandro-21 <alejandrodurillovargas21@gmail.com>
2026-05-20 12:20:08 +0200

    final com

--- js/javascript.js
+++ js/javascript.js
@@ -3,7 +3,7 @@ const PORT = 3000;
 const ENDPOINT_SERVER = "http://localhost";
 //Variables
 let imagenmostrada = 0;
-
+let videomostrado = 0;
 //Animación del ocapacity (es para que aparezca la página) 
 setTimeout(() => {
     const contenedor = document.querySelector('body');
@@ -23,6 +23,9 @@ document.addEventListener("DOMContentLoaded", function () {
     //Para las imagenes
     const btnCambiarAtributo = document.getElementById("btnCambiarAtributo");
     const imagencambiar = document.getElementById("imagen-cambiar");
+    //Para los videos
+    const btnCambiarVideo = document.getElementById("btnCambiarVideo");
+    const videocambiar = document.getElementById("video-cambiar");
     //Para los botones de cada pestaña
     const botonMostrarTodo = document.getElementById("botonmostrartodo");
     const botonBuscar = document.getElementById("botonbusquedaporproducto");
@@ -34,6 +37,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const botonActualizar = document.getElementById("botonactualizarproducto");
     const salida = document.getElementById("mensajesalida");
     const pagina = window.location.pathname;
+    //Parte de la api externa DOM
+    const endpoint_frases = "https://thesimpsonsapi.com/api/characters";
+    const frases = document.querySelector(".boton-frase");
+    const resultado = document.querySelector(".resultado-frase");
+
     let ENDPOINT_DATOS = "";
 
     if (pagina.includes("badulaque")) {
@@ -49,6 +57,9 @@ document.addEventListener("DOMContentLoaded", function () {
         ENDPOINT_DATOS = "personajes";
     }
 
+    /**
+    * @brief Parte del cambio de lenguajes, es decir, hay un botón que cada vez de pulsarlo te devuelve una imagen de un lenguaje de programación diferente utilizados.
+    */
     function lenguajes() {
         const imagenes = [
             "../imagenes/css.webp",
@@ -61,6 +72,59 @@ document.addEventListener("DOMContentLoaded", function () {
         imagenmostrada = (imagenmostrada + 1) % 5;
     }
 
+ /**
+  * @brief Funcion para cambiar de clips, a alegir entre el viejo Judio en calzoncillos, la cancion del Badulaque y la del monorail
+  */
+    function videos() {
+        const videos = [
+            "../videos/monorail.mp4",
+            "../videos/quienquiereunbadulaque.mp4",
+            "../videos/yeguagris.mp4",
+        ];
+      videocambiar.src = videos[videomostrado];
+        videomostrado = (videomostrado + 1) % videos.length;
+    }
+
+
+    /**
+     * @brief Saca una frase aleatoria de Los Simpsons (API)
+     */
+    function FrasesSimpsons() {
+        fetch(endpoint_frases)
+            .then(response => {
+                // STATUS HTTP
+                // 200 OK
+                // 404 no existe
+                // 500 error servidor
+                if (!response.ok) {
+                    throw new Error("Error al obtener la frase");
+                }
+                return response.json();
+            })
+            .then(data => {
+                // En la API hay arrays, entonces tengo que irme al apartado results para obtener la información necesaria
+                // Los personajes están en data.results, NO en data directamente
+                const lista = data.results;
+                // Escoge un personaje aleatorio del array de la api esa
+                const id = lista[Math.floor(Math.random() * lista.length)];
+                // Elige una frase aleatoria del array de PHRASES por que es un array con subsarrays
+                const frase = id.phrases[Math.floor(Math.random() * id.phrases.length)];
+                //Decoración html
+                resultado.innerHTML = `
+                    <div class="frases">
+                        <h3>${id.name}</h3>
+                        <p>"${frase}"</p>
+                    </div>
+                `;
+            })
+            .catch(error => {
+                resultado.innerHTML = `<p>${error.name}: ${error.message}</p>`;
+            });
+    }
+
+    /**
+     * @brief Función para mostrar los datos obtenidos de la base de datos en el HTML
+     */
     function mostrarDatos(productos) {
         salida.innerHTML = "";
 
@@ -97,6 +161,10 @@ document.addEventListener("DOMContentLoaded", function () {
             });
         }
     }
+    
+    /**
+     * @brief Consulta datos al servidor con filtro (el nombre)
+     */
     function consultarDatos(filtro = "todos", valor = "") {
         const ENDPOINT_SERVER_PUERTO = new URL(ENDPOINT_SERVER);
         ENDPOINT_SERVER_PUERTO.port = PORT;
@@ -129,6 +197,9 @@ document.addEventListener("DOMContentLoaded", function () {
             });
     }
 
+    /**
+     * @brief Función para insertar un nuevo producto, personaje en la base de datos
+     */
     function insertarDato(dato) {
         const ENDPOINT_SERVER_PUERTO = new URL(ENDPOINT_SERVER);
         ENDPOINT_SERVER_PUERTO.port = PORT;
@@ -159,6 +230,9 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
+    /**
+     * @brief Función para eliminar un producto o personaje de la base de datos
+     */
     function eliminarProducto(producto) {
         const ENDPOINT_SERVER_PUERTO = new URL(ENDPOINT_SERVER);
         ENDPOINT_SERVER_PUERTO.port = PORT;
@@ -191,6 +265,9 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
+    /**
+     * @brief Función para actualizar un producto o personaje en la base de datos 
+     */
     function actualizarDato(producto) {
         const ENDPOINT_SERVER_PUERTO = new URL(ENDPOINT_SERVER);
         ENDPOINT_SERVER_PUERTO.port = PORT;
@@ -226,21 +303,41 @@ document.addEventListener("DOMContentLoaded", function () {
             salida.innerHTML = `<p><b>Error</b>: ${error}</p>`;
         });
     }
-
+    //Alejandro hay una pagina que se llama desarrollo web que te sale cosas interesante ahí es donde he visto el tema de los sonidos y lo he ajustado un poco nada más. Por si te interesa
+    //Eventos:
+    //Botón para cambiar la imagen de los lenguajes de programación
     if (btnCambiarAtributo) btnCambiarAtributo.addEventListener("click", lenguajes);
-    if (botonMostrarTodo) botonMostrarTodo.addEventListener("click", () => consultarDatos());
-    //Busca por nombre de los personajes
+    btnCambiarVideo.addEventListener("click", videos);
+    //Botón para mostrar una frase aleatoria de los simpsons
+    if (frases) frases.addEventListener("click", FrasesSimpsons);
+    //Botón para mostrar todos los datos
+    if (botonMostrarTodo) botonMostrarTodo.addEventListener("click", () => {
+        //Esto es algo extra que no aparece en los documentos, es para añadir un sonido al interactuar con un botón. Hay que pulsar varias veces nunca a la primera funciona, no se porqué.
+        const sonido = new Audio("../audio/homer.mp3");
+        sonido.play();
+    consultarDatos();});
+    //Busca por filtro (nombre) de los personajes
     if (botonBuscar) botonBuscar.addEventListener("click", () => {
+        //Esto es algo extra que no aparece en los documentos, es para añadir un sonido al interactuar con un botón. Hay que pulsar varias veces nunca a la primera funciona, no se porqué.
+        const sonido = new Audio("../audio/apu.mp3");
+        sonido.play();
     consultarDatos("nombre", busquedapornombre.value.trim());
     });
-    //Busca por nombre de los productos
+    //Busca por filtro (nombre) de los productos
     if (botonBuscar) botonBuscar.addEventListener("click", () => {
+        //Esto es algo extra que no aparece en los documentos, es para añadir un sonido al interactuar con un botón. Hay que pulsar varias veces nunca a la primera funciona, no se porqué.
+        const sonido = new Audio("../audio/apu.mp3");
+        sonido.play();
     consultarDatos("nombre", busquedapornombre_producto.value.trim());
     });
-    //Busca por nombre de comic
+    //Busca por filtro (nombre) de los comics
     if (botonBuscar) botonBuscar.addEventListener("click", () => {
+        //Esto es algo extra que no aparece en los documentos, es para añadir un sonido al interactuar con un botón. Hay que pulsar varias veces nunca a la primera funciona, no se porqué.
+        const sonido = new Audio("../audio/apu.mp3");
+        sonido.play();
     consultarDatos("nombre", busquedapornombre_comic.value.trim());
     });
+    //Botón para insertar un nuevo producto/personaje/comic en la base de datos, además se le puede modificar lo que quieras insertar
     if (botonInsertar) botonInsertar.addEventListener("click", () => {
         let producto;
 
@@ -267,10 +364,12 @@ document.addEventListener("DOMContentLoaded", function () {
                 nombre_producto: "Prueba001"
             };
         }
-
+        //Esto es algo extra que no aparece en los documentos, es para añadir un sonido al interactuar con un botón. Hay que pulsar varias veces nunca a la primera funciona, no se porqué.
+        const sonido = new Audio("../audio/moe.mp3");
+        sonido.play();
         insertarDato(producto);
     });
-
+    //Botón para eliminar un nuevo producto/personaje/comic en la base de datos, además se le puede modificar lo que quieras eliminar
     if (botonEliminar) botonEliminar.addEventListener("click", () => {
         let producto;
 
@@ -283,17 +382,19 @@ document.addEventListener("DOMContentLoaded", function () {
                 codigo: 1111234
             };
         }
-
+        //Esto es algo extra que no aparece en los documentos, es para añadir un sonido al interactuar con un botón. Hay que pulsar varias veces nunca a la primera funciona, no se porqué.
+        const sonido = new Audio("../audio/bart.mp3");
+        sonido.play();
         eliminarProducto(producto);
     });
-
+    //Botón para actualizar un nuevo producto/personaje/comic en la base de datos, además se le puede modificar lo que quieras actualizar
     if (botonActualizar) botonActualizar.addEventListener("click", () => {
         let producto;
 
         if (pagina.includes("personajes")) {
             producto = {
                 codigo_personaje: 2,
-                nombre: "PruebaNombrea123",
+                nombre: "PruebanombreActualizado",
                 apellido1: "PruebaApellido",
                 madre: "PruebaMadre",
                 padre: "PruebaPadre",
@@ -313,8 +414,9 @@ document.addEventListener("DOMContentLoaded", function () {
                 nombre_producto: "ProductoActualizado"
             };
         }
-
+        //Esto es algo extra que no aparece en los documentos, es para añadir un sonido al interactuar con un botón. Hay que pulsar varias veces nunca a la primera funciona, no se porqué.
+        const sonido = new Audio("../audio/miau.mp3");
+        sonido.play();
         actualizarDato(producto);
     });
-
 });
